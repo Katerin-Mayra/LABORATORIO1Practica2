90592ea40d2e7113ff857831439ab633e16fbb8b
Katerin-Mayra <katerin4468@gmail.com>
2020-05-17 19:41:23 -0400

    ya ta

--- products.js
+++ products.js
@@ -1,5 +1,6 @@
 class Products {
-    constructor(name, description, quantity) {
+    constructor(id,name, description, quantity) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.quantity = quantity;
@@ -7,14 +8,14 @@ class Products {
 }
 class ProductsManagement {
     constructor() {
-        this.listproducts = []
+        this.listproducts = [];
     }
     addProducts(product) {
         this.listproducts.push(product);
     }
     removeProducts(product) {
         for (var i = 0; i < this.listproducts.length; i++) {
-            if (this.listproducts[i] == product) {
+            if (this.listproducts[i]['id'] == product['id']) {
                 this.listproducts.splice(i, 1);
                 return;
             }
@@ -22,10 +23,15 @@ class ProductsManagement {
         return;
     }
     updateProducts(product, newproduct) {
+        
         for (var i = 0; i < this.listproducts.length; i++) {
-            if (this.listproducts[i] == product) {
+            //console.log(product);
+            if (this.listproducts[i]['id'] == product['id']) {
                 this.listproducts[i] = newproduct;
                 return;
+            }else{
+                console.log("NO");
+                //this.listproducts[i] = newproduct;
             }
         }
     }
@@ -35,5 +41,36 @@ class ProductsManagement {
     showFirstProducts () {
         return this.listproducts[0];
     }
+
 }
 export {Products, ProductsManagement}
+
+$( document ).ready(function() {
+    $(".btn_save").hide();
+});
+
+$(document).on('click', '.btn_save', function(event) 
+{
+    var datos = $(this).closest('tr');
+    datos.find(".btn_save").hide();
+    datos.find(".btn_edit").show();
+    datos.find('td div').each(function(index,val){
+        $(this)
+        .attr('contenteditable', 'false')
+        
+        .removeClass('bg-warning')
+        .css('padding','')
+        .css('color','black');
+        //.css('background','#FF5733'); 
+    });
+    
+    updateProducts(product, newproduct)
+    //console.log(datos);
+})
+
+$(document).on('click', '.btn_del', function(event) 
+{
+    $(this).closest('tr').remove();
+})
+
+
--- ui.js
+++ ui.js
@@ -1,30 +1,104 @@
 import {Products, ProductsManagement} from "./products.js";
 class Ui {
+    
     constructor() {
+        this.proid = document.getElementById("product_id");
         this.uiName = document.getElementById("name");
         this.uiDescription = document.getElementById("description");
         this.uiQuantity = document.getElementById("quantity");
         this.uiForm = document.getElementById("form-data");
+        //this.btnedit = document.getElementById("test");
+        this.btnedit = document.getElementsByClassName("btn_edit");
+        //this.uiForm1 = document.getElementById("form-actua");
         this.container = document.getElementById("container-table");
         this.manager =  new ProductsManagement();
-        let p1 = new Products("Pollo", "Pollo Sofia", 20);
-        let p2 = new Products("Galletas", "Galleras Mabel", 50);
-        let p3 = new Products("Pollo", "Pollo Imba", 50);
+        
+        //this.oldproduct;
+        //let btn = "<a class="btn btn-success btn-xs"> Editar</a>";
+        let p1 = new Products(1,"Pollo", "Pollo Sofia", 20);
+        let p2 = new Products(2,"Galletas", "Galleras Mabel", 50);
+        let p3 = new Products(3,"Pollo", "Pollo Imba", 50);
+
+        this.id = 4;
+
+        //this.newproduct = [];
+        //////////////////////
+        let p4 = new Products("Pollo", "Pollo 1233333", 50);
         this.manager.addProducts(p1);
         this.manager.addProducts(p2);
         this.manager.addProducts(p3);
+        //this.loadEvents();
+        this.loadTable();
         this.loadEvents();
+        ///////////////////actualizar
+        //this.manager.updateProducts(p4);
+        ///////////this.manager.updateProducts(p2);
+        ////////this.manager.updateProducts(p3);
+        //this.loadEvents1();
+
     }
     loadEvents() {
+        //AGREGAR
         this.uiForm.addEventListener("submit", (e) => {
             e.preventDefault();
             this.addProducts(
+                this.id,
+                this.uiName.value,
+                this.uiDescription.value,
+                this.uiQuantity.value);
+            this.clearForm();
+            this.id = this.id + 1
+            //$(".btn_save").hide();
+            
+        });
+        //EDITAR
+        this.btned = document.getElementById("update")
+        this.btned.addEventListener("click", (e) =>{
+            e.preventDefault();
+            for (var i = 0; i < this.manager.showProducts().length; i++) {
+                if(this.proid.value == this.manager.showProducts()[i].id)
+                {
+                    this.oldproduct = new Products(this.manager.showProducts()[i].id,this.manager.showProducts()[i].name,this.manager.showProducts()[i].description,this.manager.showProducts()[i].quantity);
+                }
+                console.log(this.oldproduct);
+            }
+            
+            let newproduct = new Products(this.proid.value,this.uiName.value, this.uiDescription.value,this.uiQuantity.value);
+            this.manager.updateProducts(this.oldproduct, newproduct);
+            this.loadTable();
+            //this.clearForm();
+        });
+            //ELIMINAR
+        this.btned = document.getElementById("remove")
+        this.btned.addEventListener("click", (e) =>{
+            e.preventDefault();
+            for (var i = 0; i < this.manager.showProducts().length; i++) {
+                if(this.proid.value == this.manager.showProducts()[i].id)
+                {
+                    this.oldproduct = new Products(this.manager.showProducts()[i].id,this.manager.showProducts()[i].name,this.manager.showProducts()[i].description,this.manager.showProducts()[i].quantity);
+                }
+                console.log(this.oldproduct);
+            }
+            
+           // let newproduct = new Products(this.proid.value,this.uiName.value, this.uiDescription.value,this.uiQuantity.value);
+            this.manager.removeProducts(this.oldproduct);
+            this.loadTable();
+            //this.clearForm();
+        
+        }); 
+    }
+    
+   /* actEvents(){
+        this.uiForm1.addEventListener("submit", (e) => {
+            e.preventDefault();
+            this.updateProducts(
                 this.uiName.value,
                 this.uiDescription.value,
                 this.uiQuantity.value);
             this.clearForm();
         });
-    }
+    }*/
+
     clearForm() {
                 this.uiName.value = "";
                 this.uiDescription.value = ""
@@ -36,19 +110,51 @@ class Ui {
             html += `
             <tr>
                 <td scope="row">
-                ${this.manager.showProducts()[i].name}</td>
-                <td>${this.manager.showProducts()[i].description}</td>
-                <td>${this.manager.showProducts()[i].quantity}</td>
+                <div>${this.manager.showProducts()[i].id}</div></td>
+                <td>
+                <div>${this.manager.showProducts()[i].name}</div></td>
+                <td><div>${this.manager.showProducts()[i].description}</div></td>
+                <td><div>${this.manager.showProducts()[i].quantity}</div></td>
+                <td><a class="btn btn-warning btn-xs btn_edit" id="test" data-toggle="collapse" aria-controls="collapseExample" data-target="#collapseExample" aria-expanded="false">Editar <a/></td>
             </tr>`;
         }
         this.container.innerHTML = html;
     }
-    addProducts(name, description, quantity) {
-        let p1 = new Products(name, description, quantity);
+    addProducts(id,name, description, quantity) {
+        let p1 = new Products(id,name, description, quantity);
         this.manager.addProducts(p1);
         this.loadTable();
 
     }
+  /*  updateProducts(name, description, quantity) {
+        let p1 = new Products(name, description, quantity);
+        this.manager.updateProducts(p1);
+        this.loadTable();
+
+    }*/
+
+
 }
+
+
 let ui = new Ui();
 ui.loadTable();
+
+$(document).on('click', '.btn_edit', function(event) 
+{
+            var datos = $(this).closest('tr');
+            var id = datos.find('td')[0].textContent;
+            var nombre = datos.find('td')[1].textContent;
+            var descripcion = datos.find('td')[2].textContent;
+            var cantidad = datos.find('td')[3].textContent;
+            document.getElementById("product_id").value = id;
+            document.getElementById("name").value = nombre;
+            document.getElementById("description").value = descripcion;
+            document.getElementById("quantity").value = cantidad;
+})
+
+//shift +alt+a
+
+
+
+/////////////////////////////////////////////////////////////////////////
